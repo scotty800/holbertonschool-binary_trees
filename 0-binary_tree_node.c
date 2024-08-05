1b81b7b0dad304e3316d11fe98bd50dbeb9e78a9
Your Name <you@example.com>
2024-08-05 13:37:38 +0000

    modification de l'exo 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,16 +8,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 
+	if (new_node == NULL)
+	
+		return (NULL);
+	
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 
 	return (new_node);
 }
