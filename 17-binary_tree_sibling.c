87557ba8a303cc25f2ef41d903efeaf8162c62e6
root <root@DESKTOP-SH55I7R>
2024-08-06 15:31:05 +0200

    Write a function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,9 +3,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - Checks if a binary tree is full
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if the tree is full, 0 otherwise. If tree is NULL, return 0.
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling
+ *
+ * Return: Pointer to the sibling node, or NULL if no sibling
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
@@ -15,6 +16,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left == node)
 		return (node->parent->right);
-	return(node->parent->left);
+	return (node->parent->left);
 
 }
