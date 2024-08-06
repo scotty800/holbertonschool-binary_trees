52cddfd7620a3160dce421201e9939c1ecc78479
Your Name <you@example.com>
2024-08-06 11:34:12 +0000

    Write a function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_help - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  * Return: Height of the tree. If tree is NULL, return 0.
  */
